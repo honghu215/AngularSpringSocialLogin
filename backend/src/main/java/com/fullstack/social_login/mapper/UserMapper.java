3a573b988834468ad61cc6aca154904170c2fde4
Hong Hu <huhong215@gmail.com>
2019-01-31 16:31:44 -0500

    added mybatis mapper

---
+++ backend/src/main/java/com/fullstack/social_login/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.fullstack.social_login.mapper;
+
+import com.fullstack.social_login.model.User;
+
+import java.util.Optional;
+
+public interface UserMapper {
+
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByEmail(String email);
+}
